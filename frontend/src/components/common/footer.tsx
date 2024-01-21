438704fe397b93ecae5d4f730798b80fac652cea
kurumimochi <s.ymyuuuu@gmail.com>
2024-01-22 00:42:59 +0900

    feat: #8 フッターを追加

--- frontend/src/components/common/footer.tsx
+++ frontend/src/components/common/footer.tsx
@@ -1,3 +1,48 @@
-export function Footer() {
-  return <></>;
+"use client";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Slide from "@mui/material/Slide";
+import AppBar from "@mui/material/AppBar";
+import { Container, useScrollTrigger } from "@mui/material";
+
+interface Props {
+  window?: () => Window;
+}
+
+export function Footer(props: Props) {
+  const window = props.window;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <>
+      <Box>
+        <Stack
+          component='footer'
+          direction='row'
+          sx={{
+            color: "white",
+            width: "100%",
+            bgcolor: "#2E3036",
+            p: "10px",
+          }}
+        >
+          <Container sx={{textAlign: 'center'}}>
+          <Typography
+            style={{
+              fontFamily: "Roboto",
+              fontSize: "12px",
+              fontWeight: 500,
+            }}
+          >
+            © 2024 神奈総23期有志
+          </Typography>
+          </Container>
+        </Stack>
+      </Box>
+    </>
+  );
 }
