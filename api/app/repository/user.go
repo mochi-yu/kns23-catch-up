19d9ec143e5c2b9599dc414b0f0234206fdc138d
mochi-yu <mochi.2225.it@gmail.com>
2024-02-13 16:26:59 +0900

    feat: DynamoDBの新規ユーザのInsert処理を追加

--- api/app/repository/user.go
+++ api/app/repository/user.go
@@ -1,8 +1,19 @@
 package repository
 
-import "github.com/mochi-yu/kns23-catch-up/app/infrastructure"
+import (
+	"context"
+	"fmt"
+
+	"github.com/mochi-yu/kns23-catch-up/app/infrastructure"
+	"github.com/mochi-yu/kns23-catch-up/app/model"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
 
 type UserRepository interface {
+	InsertNewUser(u model.UserModel) error
 }
 
 type userRepository struct {
@@ -11,4 +22,22 @@ type userRepository struct {
 
 func NewUserRepository(dynamodb infrastructure.DynamoDBClient) UserRepository {
 	return &userRepository{db: &dynamodb}
+}
+
+func (ur *userRepository) InsertNewUser(u model.UserModel) error {
+	// DBのキーとマッピング
+	av, err := attributevalue.MarshalMap(u)
+	if err != nil {
+		return fmt.Errorf("failed marshal db attribute: %v", err)
+	}
+
+	// インサート処理を実施
+	if _, err = ur.db.C.PutItem(context.Background(), &dynamodb.PutItemInput{
+		TableName: aws.String("users"),
+		Item:      av,
+	}); err != nil {
+		return fmt.Errorf("failed to put new user: %v", err)
+	}
+
+	return nil
 }
--- api/go.mod
+++ api/go.mod
@@ -6,15 +6,17 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.24.1
 	github.com/aws/aws-sdk-go-v2/config v1.26.4
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.15
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.26.9
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.27.1
 	github.com/gin-gonic/gin v1.9.1
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.12.17 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.18.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.8.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
