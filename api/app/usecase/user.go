c5a25d0d3337b13fa32e4f958fa5ce40597f3d54
mochi-yu <mochi.2225.it@gmail.com>
2024-02-15 01:20:49 +0900

    feat: GET /authのユースケース処理を追加

--- api/app/usecase/user.go
+++ api/app/usecase/user.go
@@ -1,12 +1,18 @@
 package usecase
 
 import (
+	"firebase.google.com/go/v4/auth"
 	"github.com/mochi-yu/kns23-catch-up/app/model"
 	"github.com/mochi-yu/kns23-catch-up/app/repository"
 )
 
 type UserUseCase interface {
 	RegisterPost(model.RegisterPostParam) error
+	GetAuth(u *auth.UserInfo) (
+		userInfo *model.UserModel,
+		is_temp_user bool,
+		err error,
+	)
 }
 
 type userUseCase struct {
@@ -28,4 +34,45 @@ func (uu *userUseCase) RegisterPost(param model.RegisterPostParam) error {
 	}
 
 	return nil
+}
+
+// ユーザ情報取得のエンドポイント
+func (uu *userUseCase) GetAuth(u *auth.UserInfo) (
+	userInfo *model.UserModel,
+	is_temp_user bool,
+	err error,
+) {
+	// usersテーブルからユーザを取得
+	userInfo, err = uu.r.User.GetUserByUid(u.UID)
+	if err != nil {
+		// ユーザが見つからない場合には"no user"のエラーを返す
+		if err.Error() != "no user" {
+			return nil, false, err
+		}
+	} else {
+		return userInfo, false, nil
+	}
+
+	// 取得に失敗したらtemp_usersテーブルからユーザを取得
+	tempUser, err := uu.r.User.GetTempUserByUid(u.UID)
+	if err != nil {
+		if err.Error() != "no temp_user" {
+			return nil, false, err
+		}
+	} else {
+		return model.TempUserModel2UserModel(tempUser), true, nil
+	}
+
+	// それもできなければ、新しくtemp_usersテーブルに格納
+	tempUser = &model.TempUserModel{
+		FirebaseID:  u.UID,
+		MailAddress: u.Email,
+	}
+
+	err = uu.r.User.InsertTempUser(*tempUser)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return model.TempUserModel2UserModel(tempUser), true, nil
 }
