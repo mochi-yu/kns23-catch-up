1399776611eec8dbd34f4e7684847c42a45ccbfb
mochi-yu <mochi.2225.it@gmail.com>
2024-02-21 19:11:02 +0900

    feat: ログイン時のGET/POST処理を行う関数を定義

--- frontend/src/util/axios.ts
+++ frontend/src/util/axios.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/lib/firebase/firebase";
 import axios from "axios";
 
 const instance = axios.create({
@@ -6,5 +7,33 @@ const instance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+export async function GetWithLogin(path: string): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance.get(path, {
+    headers: { Authorization: "Bearer " + idToken },
+  });
+}
+
+export async function PostWithLogin(path: string, data: any): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .post(path, data, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
 
 export default instance;
